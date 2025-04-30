9713dc9659fd3409bb730d6a8c8a96a42768cb07
perfectWar <162577707+perfectWar@users.noreply.github.com>
2025-04-30 08:54:10 +0530

    Added

---
+++ Containers/deque.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// deque stands for double ended queue 
+// more flexible, as elements can be added and removed from both ends (at the front and the back)
+// can also access elements by index numbers.
+
+void deques(){
+    deque<string> cars;
+
+    cars.push_back("BMW");
+    cout << cars[0];
+
+    // all other function is same as list and vector 
+}
+int main(){
+    deques();
+}
